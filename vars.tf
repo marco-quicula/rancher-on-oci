620c8b17e489b981afb5aeec7eb698c275e79cac
Marco Quiçula <marco.quicula@quicula.com.br>
2023-12-09 02:45:02 -0300

    Fixing terraform cloud variables

--- vars.tf
+++ vars.tf
@@ -1,3 +1,16 @@
+# Terraform variables for Terraform Cloud. This variables is required for Terraform Cloud.
+variable "TF_ORGANIZATION" {
+  description = "The name of the Terraform organization"
+  type        = string
+  default     = ""
+}
+
+variable "TF_WORKSPACE" {
+  description = "The name of the Terraform workspace"
+  type        = string
+  default     = ""
+} 
+
 # OCI Credentials.
 variable "tenancy_ocid" {
   default = ""
