8a8bc724ee853480f58c79f2c4321b83cc6d9103
Marco Quiçula <marco.quicula@quicula.com.br>
2023-12-10 16:21:49 -0300

    Create Instances

---
+++ ssh.tf
@@ -0,0 +1,16 @@
+## SSH KEYS to Instances
+resource "local_file" "ssh_private_key" {
+  content         = file(pathexpand("~/.ssh/instance_key"))
+  filename        = "id_rsa"
+  file_permission = "0600"
+}
+
+resource "local_file" "ssh_public_key" {
+  content         = file(pathexpand("~/.ssh/instance_key.pub"))
+  filename        = "id_rsa.pub"
+  file_permission = "0600"
+}
+
+locals {
+  authorized_keys = [chomp(local_file.ssh_public_key.content)]
+}
