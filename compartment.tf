913337db3e45b08491214892738b7f3c383fcc0f
Marco Quiçula <marco.quicula@quicula.com.br>
2023-12-09 21:49:14 -0300

    Fix format

--- compartment.tf
+++ compartment.tf
@@ -1,5 +1,5 @@
 #OCI Compartment
 resource "oci_identity_compartment" "compartment" {
-  name           = var.compartment_name
-  description    = var.compartment_description
+  name        = var.compartment_name
+  description = var.compartment_description
 }
