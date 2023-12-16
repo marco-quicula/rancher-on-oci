6dd2d2368c68934d291c78dc26d34a248075d560
Marco Quiçula <marco.quicula@quicula.com.br>
2023-12-16 18:40:07 -0300

    Replace rancher_installation_mode to remote on CI/CD

--- rancher.tf
+++ rancher.tf
@@ -44,7 +44,7 @@ resource "null_resource" "install_kubectl_on_remotehost" {
 
 resource "null_resource" "install_helm_on_remotehost" {
   count      = var.rancher_installation_mode == "remote" ? 1 : 0
-  depends_on = [oci_core_instance._[1]]
+  depends_on = [null_resource.install_kubectl_on_remotehost, oci_core_instance._[1]]
   connection {
     type        = "ssh"
     user        = "ubuntu"
